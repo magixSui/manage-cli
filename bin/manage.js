d9ed2b9b61175fcf50c12c79e07d5b1d8a426a8b
magixsui <sx32612254@gmail.com>
2019-07-05 08:08:47 +0800

    refector

--- bin/manage.js
+++ bin/manage.js
@@ -1,9 +1,38 @@
 #!/usr/bin/env node
+const program = require('commander')
 
-const commander = require('commander')
-
-commander
-  .version(require('../package').version , '-v --version')
+program
+  .version(require('../package').version)
   .usage('<command> [options]')
-  .command('init','create a new project')
-  .parse(process.argv)
+
+program
+  .command('ui')
+  .description('start and open the code generator')
+  .action((cmd) => {
+    require('../lib/ui')(cleanArgs(cmd))
+  })
+
+  /**
+ * Help.
+ */
+
+function help () {
+  program.parse(process.argv)
+  if (program.args.length < 1) return program.help()
+}
+help()
+
+// commander passes the Command object itself as options,
+// extract only actual options into a fresh object.
+function cleanArgs (cmd) {
+  const args = {}
+  cmd.options.forEach(o => {
+    const key = camelize(o.long.replace(/^--/, ''))
+    // if an option is not present and Command has a method with the same name
+    // it should not be copied
+    if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
+      args[key] = cmd[key]
+    }
+  })
+  return args
+}
